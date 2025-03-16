135fe90a1be4a24e58d32800a9db99cf16080849
Rafael <mikudesudesu@vivaldi.net>
2025-03-16 01:00:25 -0300

    - Creates class to play the game 	- Just started 	- In the middle of solving why I can't compare my words to letters. Guess i'm tired now

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,16 +2,11 @@ require 'csv'
 require 'pry-byebug'
 # Dictionary to gather all the words
 class Dictionary
-  def words_generator
-    dictionary = File.open('../files/google-10000-english-no-swears.txt')
-    word_map = dictionary.readlines.map { |word| word.chomp if word.chomp.length >= 4 && word.chomp.length <= 11 }
-    word_map.compact.sample
-  end
 end
 
 # Presentation text to start the game
 module Presentation
-  puts INTRO = <<~TEXT.freeze
+  INTRO = <<~TEXT.freeze
     Welcome to the classic game of hangman
     this game will take a word with between 5 and 12
     characters, and you'll have to guess!
@@ -34,15 +29,25 @@ class PlayGame
   end
 end
 
-# class to handle all input comparisons
-class InputComparison
+# class to handle all comparisons
+class Comparison
+  def words_generator
+    dictionary = File.open('../files/google-10000-english-no-swears.txt')
+    word_map = dictionary.readlines.map { |word| word.chomp if word.chomp.length >= 4 && word.chomp.length <= 11 }
+    word_map.compact.sample
+  end
+
+  def self.hide_string(string)
+    string.chars.map { '_ ' }.join
+  end
+
   def ask_input
-    puts 'type a character'
+    'type a character'
   end
 
   def player_input
     loop do
-      ask_input
+      puts ask_input
       answer = gets.chomp
       valid_input?(answer)
       break answer
@@ -54,15 +59,51 @@ class InputComparison
   def valid_input?(input)
     raise InvalidInputError unless input.match?(/[a-zA-Z]/) && input.length == 1
   end
+
+  def compare_input(word, letter)
+    word.chars.map { |game_word| game_word == letter ? letter : false }
+  end
+
+  def compare_score(score)
+    score.include?(false)
+  end
+end
+
+# Class to keep and maintain score
+class Score
+  attr_accessor :score
+
+  def initialize
+    @score = 5
+  end
+
+  def wrong_answer
+    @score -= 1
+    'Game over' if @score.zero?
+  end
 end
 
 class InvalidInputError < StandardError
 end
 
 # Class to operate all the game's logic
-class Game
-  def compare_input(word, letter)
-    word.chars.map { |game_word| game_word == letter ? letter : nil }
+class Hangman
+  def initialize
+    @word = Comparison.new
+    @score = Score.new
+    @guess = Comparison.new
+  end
+
+  def game_start
+    puts PlayGame.intro
+    @word = @word.words_generator
+    puts "#{@word} is the word"
+    puts Comparison.hide_string(@word)
+    loop do
+      @guess = @guess.player_input
+      @guess.compare_input(@word, @guess)
+    end
   end
 end
 
+Hangman.new.game_start
